fed3ff9554d7c963aa2301e5fa8295b5a9dcc3fb
ShameemudheenMP <shameemmanalaya26@gmail.com>
2022-07-05 07:54:16 +0530

    guest view added

--- Borokaro/main/urls.py
+++ Borokaro/main/urls.py
@@ -4,7 +4,7 @@ from .views import log_in, sign_up, home
 
 urlpatterns = [
     path('home/', home, name='home'),
-    # path('home/', home, name='home'),
+    # path('', home, name='home'),
     path('signup/', sign_up, name='signup'),
     path('login/', log_in, name='login')
 ]
